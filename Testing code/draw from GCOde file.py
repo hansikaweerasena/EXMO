368e01856faf8fe1ac96433b1c84d904dfb6389d
Hansika Madushan Weerasena <hansikam94@gmail.com>
2017-05-01 17:16:52 +0530

    Update draw from GCOde file.py

--- Testing code/draw from GCOde file.py
+++ Testing code/draw from GCOde file.py
@@ -1,6 +1,10 @@
 from draw import plot
 
-port = "COM3"
+parser = argparse.ArgumentParser(description='This is a basic gcode sender. http://crcibernetica.com')
+parser.add_argument('-p','--port',help='Input USB port',required=True)
+parser.add_argument('-f','--file',help='Gcode file name',required=True)
+args = parser.parse_args()
+
 # plot("1.g",port)
 # plot("2.g",port)
 # plot("3.g",port)
@@ -9,6 +13,6 @@ port = "COM3"
 # plot("6.g",port)
 # plot("7.g",port)
 # plot("8.g",port)
-plot("5.g",port)
+plot(args.file,args.port)
 
 
